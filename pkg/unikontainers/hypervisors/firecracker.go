7300fa3bbb0c678131a72d04c5fae3efe7e9efa3
Charalampos Mainas <cmainas@nubificus.co.uk>
2025-07-01 09:45:00 +0000

    merge first
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- pkg/unikontainers/hypervisors/firecracker.go
+++ pkg/unikontainers/hypervisors/firecracker.go
@@ -138,13 +138,6 @@ func (fc *Firecracker) Execve(args ExecArgs, _ unikernels.Unikernel) error {
 		}
 		FCDrives = append(FCDrives, aBlock)
 	}
-	// TODO: Check if this check causes any performance drop
-	// or explore alternative implementations
-	if runtime.GOARCH == "arm64" {
-		consoleStr := " console=ttyS0"
-		args.Command += consoleStr
-	}
-
 	FCSource := FirecrackerBootSource{
 		ImagePath:  args.UnikernelPath,
 		BootArgs:   args.Command,
